bf54e10b086ffc72e174f76a6c1a6061bcd7ff8e
JoaoGMagri <jgmagri@hotmail.com>
2022-12-20 23:21:28 -0300

    feat: get urls com ids

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -16,8 +16,34 @@ export async function postShorten(req, res) {
             VALUES
                 ($1, $2, $3, $4);
         `,
+
         [objSession.rows[0].idUser, shortUrl, url, 0]);
         res.sendStatus(201);
+
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+
+}
+export async function getShorten(req, res) {
+
+    const {id} = req.params
+    try {
+
+        const obj = await connection.query(`
+            SELECT 
+                id, 
+                "shortUrl", 
+                url 
+            FROM
+                urls 
+            WHERE
+                id=$1
+            ;
+        `,
+        [id]);
+        res.send(obj.rows[0]).status(200);
 
     } catch (error) {
         console.log(error);
--- src/routers/urls.routers.js
+++ src/routers/urls.routers.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { postShorten } from "../controllers/urls.controllers.js";
+import { postShorten, getShorten } from "../controllers/urls.controllers.js";
 import { authorization } from "../middleware/authorization.middleware.js";
 
 const router = Router();
 
 router.post("/urls/shorten", authorization, postShorten);
+router.get("/urls/:id", getShorten);
 
 export default router;

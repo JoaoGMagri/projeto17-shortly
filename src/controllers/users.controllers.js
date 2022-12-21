af943002677029e861f92a1242af82f8fa084834
JoaoGMagri <jgmagri@hotmail.com>
2022-12-21 18:27:45 -0300

    feat: ranking

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -6,21 +6,22 @@ export async function getUsersMe(req, res) {
 
     try {
 
-
         const obj = await connection.query(`
         SELECT 
             users.id,
             users.name,
             COALESCE(SUM (urls."visitCount"), 0)::INTEGER as "visitCount",
             COALESCE(
-            array_agg(
-                json_build_object(
-                    'id', urls.id, 
-                    'url', urls.url, 
-                    'shortUrl', urls."shortUrl", 
-                    'visitCount', urls."visitCount") 
-            ORDER BY urls."visitCount") 
-            FILTER (where urls.id is not null), ARRAY[]::json[]) as "shortenedUrls"
+                array_agg(
+                    json_build_object(
+                        'id', urls.id, 
+                        'url', urls.url, 
+                        'shortUrl', urls."shortUrl", 
+                        'visitCount', urls."visitCount"
+                    ) 
+                ORDER BY urls."visitCount") 
+                FILTER (where urls.id is not null), ARRAY[]::json[]
+            ) as "shortenedUrls"
         FROM 
             users
         LEFT JOIN 
@@ -43,37 +44,28 @@ export async function getUsersMe(req, res) {
 
 export async function getRanking(req, res) {
 
-    const { idUser } = req.userExists.rows[0];
-
     try {
-
 
         const obj = await connection.query(`
         SELECT 
-            users.id,
-            users.name,
-            COALESCE(SUM (urls."visitCount"), 0)::INTEGER as "visitCount",
-            COALESCE(
-            array_agg(
-                json_build_object(
-                    'id', urls.id, 
-                    'url', urls.url, 
-                    'shortUrl', urls."shortUrl", 
-                    'visitCount', urls."visitCount") 
-            ORDER BY urls."visitCount") 
-            FILTER (where urls.id is not null), ARRAY[]::json[]) as "shortenedUrls"
+            users.id, 
+            users.name, 
+            COUNT(urls."shortUrl") AS "linksCount", 
+            COALESCE(SUM(urls."visitCount"), 0) AS "visitCount" 
         FROM 
-            users
+            users 
         LEFT JOIN 
-            urls
+            urls 
         ON 
-            users.id=urls."idUser"
-        WHERE 
-            users.id=$1
+            users.id = urls."idUser" 
         GROUP BY 
-            users.id;
-        `, [idUser]);
-        res.send(obj.rows[0]).status(200);
+            users.id 
+        ORDER BY 
+            "visitCount" DESC, 
+            "linksCount" DESC 
+            LIMIT 10;
+        `);
+        res.send(obj.rows).status(200);
 
     } catch (error) {
         console.log(error);
